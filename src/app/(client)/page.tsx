04a077afc9c30d1fc50e1191242a6f19422d76d1
MatiasAlemany <matiasalemany2@gmail.com>
2025-04-29 11:29:28 +0200

    add banner home

--- src/app/(client)/page.tsx
+++ src/app/(client)/page.tsx
@@ -1,12 +1,39 @@
 import { getProducts } from "@/_actions/actions";
-import { ProductCard } from "@/components/product-card";
+import banner1 from "../../../public/banner1.jpeg";
+import Image from "next/image";
 
 export default async function Home() {
   const products = await getProducts();
   return (
-    <div className="flex flex-col items-center justify-center w-full h-screen">
-      <h1>K3Y-SHOP</h1>
-      <h2 className="mt-12">PROXIMAMENTE...</h2>
-    </div>
+    <>
+      {/* BANNER RESPONSIVE */}
+      <div className="w-screen overflow-hidden">
+        {/* DESKTOP & TABLET */}
+        <div className="hidden sm:block w-full h-[400px] relative lg:h-[800px]">
+          <Image
+            src={banner1}
+            alt="Banner horizontal"
+            fill
+            className="object-fill"
+            priority
+          />
+        </div>
+        {/* MÓVIL */}
+        <div className="block sm:hidden w-screen h-[100vw] relative">
+          <Image
+            src={banner1}
+            alt="Banner vertical"
+            fill
+            className="object-center rotate-90 origin-center"
+            priority
+          />
+        </div>
+      </div>
+
+      <div className="flex flex-col items-center justify-center w-full h-screen">
+        <h1>K3Y-SHOP</h1>
+        <h2 className="mt-12">PROXIMAMENTE...</h2>
+      </div>
+    </>
   );
 }
